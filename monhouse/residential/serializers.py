58c4a72306f80eaf0e0651a7dd3a41490911ada6
Priyanka <36852314+gowripriyanka@users.noreply.github.com>
2020-08-22 22:10:09 +0530

    Update serializers.py

--- monhouse/residential/serializers.py
+++ monhouse/residential/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 from .models import *
-class PropertySerializer(models.ModelSerializers):
+class PropertySerializer(serializers.ModelSerializers):
     class Meta:
         model = PropertyDetail
         fields = '__all__'
 
-class LocationSerializer(models.ModelSerializers):
+class LocationSerializer(serializers.ModelSerializers):
     class Meta:
         model = LocationDetail
         fields = '__all__'
 
-class RentalSerializer(models.ModelSerializers):
+class RentalSerializer(serializers.ModelSerializers):
     class Meta:
         model = RentalDetail
         fields = '__all__'
